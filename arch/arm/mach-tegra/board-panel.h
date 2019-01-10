792214b10260c79dba627f99827e645364c9b08c
Matt Gorski <matt.gorski@gmail.com>
2019-01-10 09:31:34 -0500

    Add initial support for Google Yellowstone Google Tango aka yellowstone is a 7 inch tablet similar to the shieldtablet tn8

--- arch/arm/mach-tegra/board-panel.h
+++ arch/arm/mach-tegra/board-panel.h
@@ -64,6 +64,7 @@ extern struct tegra_panel_ops dsi_s_wqxga_10_1_ops;
 
 extern struct tegra_panel dsi_l_720p_5;
 extern struct tegra_panel dsi_j_720p_4_7;
+extern struct tegra_panel dsi_j_wuxga_7;
 extern struct tegra_panel dsi_s_1080p_5;
 extern struct tegra_panel dsi_p_wuxga_10_1;
 extern struct tegra_panel dsi_a_1080p_11_6;
